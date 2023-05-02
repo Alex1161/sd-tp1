76eb8bf7ba4a1fae250418e62458167dc770b083
Alex <garbieto@fi.uba.ar>
2023-05-02 07:30:35 -0300

    trips filtered by raining days

--- rain_filter/rain_filter.py
+++ rain_filter/rain_filter.py
@@ -6,8 +6,8 @@ EOF = b'EOF'
 EOD = b'EOD'
 LOADING = 0
 PROCESSING = 1
-COLUMNS_WEATHER = ['date', 'precto']
-COLUMNS_TRIPS = ['start_ts', 'duration']
+COLUMNS_WEATHER = ['date', 'prectot']
+COLUMNS_TRIPS = ['start_date', 'duration_sec']
 
 
 class RainFilter():
@@ -30,6 +30,7 @@ class RainFilter():
         if self._state == LOADING:
             if body == EOF:
                 self._state = PROCESSING
+                self._weather['date'] = pd.to_datetime(self._weather['date'])
                 logging.info(f'action: EOF_detected | result: success | data: {self._weather}')
                 return
 
@@ -43,16 +44,24 @@ class RainFilter():
 
             self._persist(rows_date_cured)
 
-            logging.debug(f'action: rain_filter_filtering | result: success | msg_filtered: {body}')
+            logging.debug(f'action: rain_filter_loading | result: success | loaded: {body}')
         elif self._state == PROCESSING:
-            if body == EOD:
-                logging.debug(f'action: EOD_detected | result: success')
+            if body == EOF:
+                logging.info(f'action: EOF_detected | result: success')
                 return
 
-            # rows_str = body.decode('utf-8')
-            # rows = [row.split(',') for row in rows_str.split('\n')]
-            # trips = pd.DataFrame(rows, columns=COLUMNS_TRIPS)
-            # logging.info(f'{trips}')
+            rows_str = body.decode('utf-8')
+            rows = [row.split(',') for row in rows_str.split('\n')]
+            trips = pd.DataFrame(rows, columns=COLUMNS_TRIPS)
+            trips.loc[trips['duration_sec'].astype(float) < 0, 'duration_sec'] = 0
+            trips['start_date'] = pd.to_datetime(trips['start_date'], format='%Y-%m-%d %H:%M:%S')
+            trips['start_date'] = pd.to_datetime(trips['start_date'].dt.strftime('%Y-%m-%d'), format='%Y-%m-%d')
+            trips_rain = trips.merge(self._weather, left_on='start_date', right_on='date')[['start_date', 'duration_sec']]
+            if trips_rain.empty:
+                return
+
+            msg = trips_rain.to_csv(None, index=False, header=False)
+            logging.debug(f'action: rain_filter_filtering | result: success | msg_filtered: {msg}')
 
 
     def run(self):
